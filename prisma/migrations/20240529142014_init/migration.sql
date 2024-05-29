6ff986cbe3dbc2c19aca138438fe896ba98d25ca
Gopgap <Noppawat3984@gmail.com>
2024-05-29 21:39:14 +0700

    🛢   migrated DB

---
+++ prisma/migrations/20240529142014_init/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "userProfile" TEXT;
--- src/db/db.service.ts
+++ src/db/db.service.ts
@@ -4,7 +4,12 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class DbService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-    await this.$connect();
-    console.log('✅ Connnected DB');
+    try {
+      await this.$connect();
+
+      console.log('✅ Connnected DB');
+    } catch (error) {
+      console.log('Connect DB failed');
+    }
   }
 }
